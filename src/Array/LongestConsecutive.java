9f1e11d0aa7070f14f35732129090e1e41d4b2bc
zhangdapeng <1099469985@qq.com>
2017-12-27 21:49:25 +0800

    Leetcode

---
+++ src/Array/LongestConsecutive.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutive {
+    public int solution(int[] nums){
+        if (nums == null || nums.length <= 0 )
+            return 0;
+        Set<Integer> set = new HashSet<Integer>();
+        for (int num : nums) set.add(num);
+
+        int max = 1;
+        for (int num : nums){
+            if(set.remove(num)){
+                int val = num;
+                int sum = 1;
+                while (set.remove(val-1)) val--;
+                sum += num - val;
+
+                val = num ;
+                while(set.remove(val+1)) val++;
+                sum += val - num;
+
+                max = Math.max(max,sum);
+            }
+        }
+        return max;
+    }
+}
--- src/Array/test.java
+++ src/Array/test.java
@@ -2,11 +2,17 @@ package Array;
 
 public class test {
     public static void main(String[] args) {
-        int sums[] = {1};
+        //int sums[] = {1,3,1,1,1};
+        int sums1[] = {};
+        int sums2[] = {4,5,6,2,3,7,9,13,54};
 /*        RemoveDuplicates2 Rp= new RemoveDuplicates2();
         System.out.println(Rp.solution(sums));*/
 
-        SearchRotated SR = new SearchRotated();
-        System.out.println(SR.soution(sums,1));
+        //SearchRotated SR = new SearchRotated();
+        //SearchRotated2 SR = new SearchRotated2();
+        //MedianOfTwoSorted MOT = new MedianOfTwoSorted();
+        LongestConsecutive LC = new LongestConsecutive();
+        System.out.println(LC.solution(sums2));
+
     }
 }
